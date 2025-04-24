69fcffce247f0c0d4ef0c3917249013f2cd36d8c
Шоколад <136274120+twnesss@users.noreply.github.com>
2025-04-25 03:30:50 +0700

    Build

--- config/config.go
+++ config/config.go
@@ -447,55 +447,47 @@ func Parse(buf []byte) (*Config, error) {
 
 func DefaultRawConfig() *RawConfig {
 	return &RawConfig{
-		AllowLan:          false,
+		AllowLan:          true,
 		BindAddress:       "*",
 		LanAllowedIPs:     []netip.Prefix{netip.MustParsePrefix("0.0.0.0/0"), netip.MustParsePrefix("::/0")},
-		IPv6:              true,
+		IPv6:              false,
 		Mode:              T.Rule,
 		GeoAutoUpdate:     false,
 		GeoUpdateInterval: 24,
 		GeodataMode:       geodata.GeodataMode(),
 		GeodataLoader:     "memconservative",
-		UnifiedDelay:      false,
+		UnifiedDelay:      true,
 		Authentication:    []string{},
-		LogLevel:          log.INFO,
+		LogLevel:          log.SILENT,
 		Hosts:             map[string]any{},
 		Rule:              []string{},
 		Proxy:             []map[string]any{},
 		ProxyGroup:        []map[string]any{},
-		TCPConcurrent:     false,
+		TCPConcurrent:     true,
 		FindProcessMode:   P.FindProcessStrict,
 		GlobalUA:          "clash.meta/" + C.Version,
 		ETagSupport:       true,
 		DNS: RawDNS{
-			Enable:         false,
+			Enable:         true,
 			IPv6:           false,
 			UseHosts:       true,
 			UseSystemHosts: true,
-			IPv6Timeout:    100,
+			IPv6Timeout:    60,
 			EnhancedMode:   C.DNSMapping,
 			FakeIPRange:    "198.18.0.1/16",
 			FallbackFilter: RawFallbackFilter{
-				GeoIP:     true,
-				GeoIPCode: "CN",
+				GeoIP:     false,
+				GeoIPCode: "",
 				IPCIDR:    []string{},
 				GeoSite:   []string{},
 			},
-			DefaultNameserver: []string{
-				"114.114.114.114",
-				"223.5.5.5",
-				"8.8.8.8",
-				"1.0.0.1",
+			// DefaultNameserver: []string{},
+			// NameServer: []string{},
+			ProxyServerNameserver: []string{
+				"112.215.198.248", // xlsmart
+				"192.168.156.30", // telkomsel
 			},
-			NameServer: []string{
-				"https://doh.pub/dns-query",
-				"tls://223.5.5.5:853",
-			},
-			FakeIPFilter: []string{
-				"dns.msftnsci.com",
-				"www.msftnsci.com",
-				"www.msftconnecttest.com",
-			},
+			// FakeIPFilter: []string{},
 			FakeIPFilterMode: C.FilterBlackList,
 		},
 		NTP: RawNTP{
@@ -509,7 +501,7 @@ func DefaultRawConfig() *RawConfig {
 			Enable:              false,
 			Device:              "",
 			Stack:               C.TunGvisor,
-			DNSHijack:           []string{"0.0.0.0:53"}, // default hijack all dns query
+			DNSHijack:           []string{"any:53"}, // default hijack all dns query
 			AutoRoute:           true,
 			AutoDetectInterface: true,
 			Inet6Address:        []netip.Prefix{netip.MustParsePrefix("fdfe:dcba:9876::1/126")},
